f3aea91ff4d79f5f3f09b0645078b409142f96b5
AIRONAX Developer <aironax@outlook.com>
2026-09-02 20:21:48 +0530

    refactor(hr): use typed zrefs for employee cross-module relations
    Replace untyped Many2One fields with CoreUser, CorePartner, and CoreCompany
    aliases in hr_employee.

---
+++ hr/models/zrefs.go
@@ -0,0 +1,9 @@
+package models
+
+import (
+	basemodels "sumeru/addons/base/models"
+)
+
+type CorePartner = basemodels.CorePartner
+type CoreCompany = basemodels.CoreCompany
+type CoreUser = basemodels.CoreUser
